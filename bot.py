a3c1b0640d4531e67159d3987c8f76dd8ba814df
misharoyzmann94 <misharoyzmann94@gmail.com>
2026-03-01 11:01:28 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,14 +31,16 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     if text.endswith("?"):
+    if random.random() < 0.5:   # 50% шанс ответа
         await update.message.reply_text("это вопрос")
-        return
+    return
 
     if text.endswith("."):
-        await update.message.reply_text("это не вопрос")
-        return
+    await update.message.reply_text("это не вопрос")
+    return
 
     if text.endswith("!"):
+    if random.random() < 0.5:   # 50% шанс ответа
         await update.message.reply_text("не ори")
 
 # --- Flask сервер для Render ---
@@ -62,3 +64,4 @@ if __name__ == "__main__":
     app.add_handler(MessageHandler(filters.ALL, handle_message))
     print("BOT STARTED")
     app.run_polling()
+
